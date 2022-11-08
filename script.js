2350ed98a916571955e57479aa52866139b8fe6c
Manuel Casanova <manucasanova@hotmail.com>
2022-11-08 10:28:27 -0800

    Center map in current location

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFudWVsY2FzYW5vdmEiLCJhIjoiY2xhOGhxMHFwMDFmbzNvcXUzZzR4NzlqbiJ9.hR-e24cPACWJj8mOaWU9SA';
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
-});
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
+
+function successLocation(position) {
+  console.log(position)
+  //Position the map here
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+
+}
+
+function setupMap(center) {
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: center,
+    zoom: 12
+  });
+}
+
