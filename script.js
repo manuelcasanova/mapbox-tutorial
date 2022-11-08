d8e612632dd7f3f1e11f1481772353e88e3a63a7
Manuel Casanova <manucasanova@hotmail.com>
2022-11-08 10:06:24 -0800

    Display map

---
+++ script.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFudWVsY2FzYW5vdmEiLCJhIjoiY2xhOGhxMHFwMDFmbzNvcXUzZzR4NzlqbiJ9.hR-e24cPACWJj8mOaWU9SA';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
